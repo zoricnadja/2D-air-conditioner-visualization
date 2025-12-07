78c1fde8c7ef061a9e1f5425f256003fcdc5d338
Nadja Zoric <zoric.n2003@gmail.com>
2025-12-07 04:35:15 +0100

    feat: Add screens

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,8 +2,8 @@
 #include <GLFW/glfw3.h>
 
 #define _USE_MATH_DEFINES
-#include <cmath> // za pi
-#include <algorithm> // za max()
+#include <cmath> 
+#include <algorithm> 
 #include <iostream>
 #include "../Header/Util.h"
 #include "../Header/Callbacks.h"
@@ -11,12 +11,9 @@
 #include "../Header/Circle/Lamp.h"
 
 const int NUM_SLICES = 50;
+const int NUM_OF_DIGITS = 10;
+const int NUM_OF_SYMBOLS = 3;
 
-// Main fajl funkcija sa osnovnim komponentama OpenGL programa
-
-// Projekat je dozvoljeno pisati počevši od ovog kostura
-// Toplo se preporučuje razdvajanje koda po fajlovima (i eventualno potfolderima) !!!
-// Srećan rad!
 GLFWcursor* remote;
 GLFWcursor* remoteUpPressed;
 GLFWcursor* remoteDownPressed;
@@ -27,6 +24,11 @@ bool downPressed = false;
 float xc = -0.53f, yc = 0.59f, r = 0.0045f;
 float uLampPower = 0.0f;
 bool isFlapMoving = false;
+int currentDigit1Screen1 = 2;
+int currentDigit2Screen1 = 4;
+int currentDigit1Screen2 = 3;
+int currentDigit2Screen2 = 0;
+int currentSymbol = 0;
 
 int main()
 {
@@ -65,15 +67,92 @@ int main()
     float acVertices[] = {
        -1.0f,  1.1f, 0.0f, 1.0f,
        -1.0f,  0.25f, 0.0f, 0.0f,
-       -0.45f,  0.25f, 1.0f, 0.0f,
-       -0.45f,  1.1f, 1.0f, 1.0f
+       -0.45f, 0.25f, 1.0f, 0.0f,
+       -0.45f, 1.1f, 1.0f, 1.0f
     };
 
     float flapVertices[] = {
-       -0.915f,  0.62f, 0.0f, 1.0f,
+       -0.915f, 0.62f, 0.0f, 1.0f,
        -0.91f,  0.585f, 0.0f, 0.0f,
        -0.54f,  0.585f, 1.0f, 0.0f,
-       -0.535f,  0.62f, 1.0f, 1.0f
+       -0.535f, 0.62f, 1.0f, 1.0f
+    };
+
+    float screen1Vertices[] = {
+       -0.91f,  0.72f, 0.0f, 1.0f,
+       -0.91f,  0.685f, 0.0f, 0.0f,
+       -0.85f,  0.685f, 1.0f, 0.0f,
+       -0.85f,  0.72f, 1.0f, 1.0f
+    };
+
+    float screen1num1Vertices[] = {
+       -0.91f,  0.72f, 0.0f, 1.0f,
+       -0.91f,  0.685f, 0.0f, 0.0f,
+       -0.895f,  0.685f, 1.0f, 0.0f,
+       -0.895f,  0.72f, 1.0f, 1.0f
+    };
+
+    float screen1num2Vertices[] = {
+       -0.895f,  0.72f, 0.0f, 1.0f,
+       -0.895f,  0.685f, 0.0f, 0.0f,
+       -0.88f,  0.685f, 1.0f, 0.0f,
+       -0.88f,  0.72f, 1.0f, 1.0f
+    };
+
+    float screen1degreeVertices[] = {
+       -0.88f, 0.72f, 0.0f, 1.0f,
+       -0.88f, 0.685f, 0.0f, 0.0f,
+       -0.865f, 0.685f, 1.0f, 0.0f,
+       -0.865f, 0.72f, 1.0f, 1.0f
+    };
+
+    float screen1celsiusVertices[] = {
+       -0.865f,  0.72f, 0.0f, 1.0f,
+       -0.865f,  0.685f, 0.0f, 0.0f,
+       -0.85f,  0.685f, 1.0f, 0.0f,
+       -0.85f,  0.72f, 1.0f, 1.0f
+    };
+
+    float screen2Vertices[] = {
+       -0.59f,  0.72f, 0.0f, 1.0f,
+       -0.59f,  0.685f, 0.0f, 0.0f,
+       -0.53f,  0.685f, 1.0f, 0.0f,
+       -0.53f,  0.72f, 1.0f, 1.0f
+    };
+    
+    float screen2num1Vertices[] = {
+       -0.59f,  0.72f, 0.0f, 1.0f,
+       -0.59f,  0.685f, 0.0f, 0.0f,
+       -0.575f,  0.685f, 1.0f, 0.0f,
+       -0.575f,  0.72f, 1.0f, 1.0f
+    };
+
+    float screen2num2Vertices[] = {
+       -0.575f,  0.72f, 0.0f, 1.0f,
+       -0.575f,  0.685f, 0.0f, 0.0f,
+       -0.56f,  0.685f, 1.0f, 0.0f,
+       -0.56f,  0.72f, 1.0f, 1.0f
+	};
+
+    float screen2degreeVertices[] = {
+       -0.56f, 0.72f, 0.0f, 1.0f,
+       -0.56f, 0.685f, 0.0f, 0.0f,
+       -0.545f, 0.685f, 1.0f, 0.0f,
+       -0.545f, 0.72f, 1.0f, 1.0f
+	};
+
+    float screen2celsiusVertices[] = {
+       -0.545f,  0.72f, 0.0f, 1.0f,
+       -0.545f,  0.685f, 0.0f, 0.0f,
+       -0.53f,  0.685f, 1.0f, 0.0f,
+       -0.53f,  0.72f, 1.0f, 1.0f
+	};
+
+    float screen3Vertices[] = {
+       -0.64f,  0.72f, 0.0f, 1.0f,
+       -0.64f,  0.685f, 0.0f, 0.0f,
+       -0.605f,  0.685f, 1.0f, 0.0f,
+       -0.605f,  0.72f, 1.0f, 1.0f
     };
 
     float lampVertices[(NUM_SLICES + 2) * 2];
@@ -88,14 +167,61 @@ int main()
 	unsigned int bgVAO, bgVBO;
 	unsigned int acVAO, acVBO;
 	unsigned int flapVAO, flapVBO;
+	unsigned int screen1VAO, screen1VBO;
+	unsigned int screen2VAO, screen2VBO;
+	unsigned int screen3VAO, screen3VBO;
+	unsigned int screen1num1VAO, screen1num1VBO;
+	unsigned int screen1num2VAO, screen1num2VBO;
+	unsigned int screen1degreeVAO, screen1degreeVBO;
+	unsigned int screen1celsiusVAO, screen1celsiusVBO;
+	unsigned int screen2num1VAO, screen2num1VBO;
+	unsigned int screen2num2VAO, screen2num2VBO;
+	unsigned int screen2degreeVAO, screen2degreeVBO;
+	unsigned int screen2celsiusVAO, screen2celsiusVBO;
+	unsigned int screen3num1VAO, screen3num1VBO;
+	unsigned int screen3num2VAO, screen3num2VBO;
+	unsigned int screen3degreeVAO, screen3degreeVBO;
+	unsigned int screen3celsiusVAO, screen3celsiusVBO;
 	unsigned int lampVAO, lampVBO;
 	initRectangles(mode->width, mode->height, bgVertices,sizeof(bgVertices), bgVAO, bgVBO);
 	initRectangles(mode->width, mode->height, acVertices, sizeof(acVertices), acVAO, acVBO);
 	initRectangles(mode->width, mode->height, flapVertices, sizeof(flapVertices), flapVAO, flapVBO);
-	initCircle(lampVertices, sizeof(lampVertices), lampVAO, lampVBO);
+	initRectangles(mode->width, mode->height, screen1Vertices, sizeof(screen1Vertices), screen1VAO, screen1VBO);
+	initRectangles(mode->width, mode->height, screen1num1Vertices, sizeof(screen1num1Vertices), screen1num1VAO, screen1num1VBO);
+	initRectangles(mode->width, mode->height, screen1num2Vertices, sizeof(screen1num2Vertices), screen1num2VAO, screen1num2VBO);
+	initRectangles(mode->width, mode->height, screen1degreeVertices, sizeof(screen1degreeVertices), screen1degreeVAO, screen1degreeVBO);
+	initRectangles(mode->width, mode->height, screen1celsiusVertices, sizeof(screen1celsiusVertices), screen1celsiusVAO, screen1celsiusVBO);
+	initRectangles(mode->width, mode->height, screen2Vertices, sizeof(screen2Vertices), screen2VAO, screen2VBO);
+	initRectangles(mode->width, mode->height, screen2num1Vertices, sizeof(screen2num1Vertices), screen2num1VAO, screen2num1VBO);
+	initRectangles(mode->width, mode->height, screen2num2Vertices, sizeof(screen2num2Vertices), screen2num2VAO, screen2num2VBO);
+	initRectangles(mode->width, mode->height, screen2degreeVertices, sizeof(screen2degreeVertices), screen2degreeVAO, screen2degreeVBO);
+	initRectangles(mode->width, mode->height, screen2celsiusVertices, sizeof(screen2celsiusVertices), screen2celsiusVAO, screen2celsiusVBO);
+	initRectangles(mode->width, mode->height, screen3Vertices, sizeof(screen3Vertices), screen3VAO, screen3VBO);
+    initCircle(lampVertices, sizeof(lampVertices), lampVAO, lampVBO);
     unsigned int bgTexture = loadTexture("Resources/background.png");
     unsigned int acTexture = loadTexture("Resources/air_conditioner.png");
 	unsigned int flapTexture = loadTexture("Resources/air_vent_grill.png");
+	unsigned int screenTexture = loadTexture("Resources/flap.jpeg");
+	unsigned int lightGreyTexture = loadTexture("Resources/light_grey.jpg");
+	unsigned int darkGreyTexture = loadTexture("Resources/dark_grey.png");
+    unsigned int numberTextures[NUM_OF_DIGITS];
+	numberTextures[0] = loadTexture("Resources/0.png");
+	numberTextures[1] = loadTexture("Resources/1.png");
+	numberTextures[2] = loadTexture("Resources/2.png");
+	numberTextures[3] = loadTexture("Resources/3.png");
+	numberTextures[4] = loadTexture("Resources/4.png");
+	numberTextures[5] = loadTexture("Resources/5.png");
+	numberTextures[6] = loadTexture("Resources/6.png");
+	numberTextures[7] = loadTexture("Resources/7.png");
+	numberTextures[8] = loadTexture("Resources/8.png");
+	numberTextures[9] = loadTexture("Resources/9.png");
+    unsigned int symbolTextures[NUM_OF_SYMBOLS];
+	symbolTextures[0] = loadTexture("Resources/flake.png");
+	symbolTextures[1] = loadTexture("Resources/done.png");
+	symbolTextures[2] = loadTexture("Resources/fire.png");
+	unsigned int degreeTexture = loadTexture("Resources/degree.png");
+	unsigned int celsiusTexture = loadTexture("Resources/celsius.png");
+
     unsigned int rectShader = createShader("Shaders/rect.vert", "Shaders/rect.frag");
 	unsigned int circleShader = createShader("Shaders/circle.vert", "Shaders/circle.frag");
     glUseProgram(rectShader);
@@ -107,36 +233,55 @@ int main()
         double initFrameTime = glfwGetTime();
 
         if (isFlapMoving) {
-			if (fill == 0.0f && !uLampPower || fill == 1.0f && uLampPower) 
+            if (fill == 0.0f && !uLampPower || fill == 1.0f && uLampPower)
                 isFlapMoving = false;
             else
-                fill += (uLampPower == 1.0f) ? 0.01f : -0.01f;
+                fill += (uLampPower == 1.0f) ? 0.001f : -0.001f;
         }
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             break;
         }
-		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS and !upPressed) {
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS and !upPressed) {
             glfwSetCursor(window, remoteUpPressed);
-			upPressed = true;
-        } 
-        if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS and !downPressed) {
-            glfwSetCursor(window, remoteDownPressed);
-			downPressed = true;
+            upPressed = true;
         }
-		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE and upPressed) {
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS and !downPressed) {
+            glfwSetCursor(window, remoteDownPressed);
+            downPressed = true;
+        }
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE and upPressed) {
             glfwSetCursor(window, remote);
-			upPressed = false;
-        } 
-        if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE and downPressed) {
-			downPressed = false;
+            upPressed = false;
+        }
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE and downPressed) {
+            downPressed = false;
             glfwSetCursor(window, remote);
         }
 
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(rectShader);
-		useFill = false;
+        useFill = false;
         drawRectangles(rectShader, bgTexture, bgVAO);
         drawRectangles(rectShader, acTexture, acVAO);
+		if (uLampPower == 1.0f) {
+            drawRectangles(rectShader, lightGreyTexture, screen1VAO);
+			drawRectangles(rectShader, lightGreyTexture, screen2VAO);
+            drawRectangles(rectShader, numberTextures[currentDigit1Screen1], screen1num1VAO);
+            drawRectangles(rectShader, numberTextures[currentDigit2Screen1], screen1num2VAO);
+            drawRectangles(rectShader, degreeTexture, screen1degreeVAO);
+            drawRectangles(rectShader, celsiusTexture, screen1celsiusVAO);
+            drawRectangles(rectShader, numberTextures[currentDigit1Screen2], screen2num1VAO);
+            drawRectangles(rectShader, numberTextures[currentDigit2Screen2], screen2num2VAO);
+            drawRectangles(rectShader, degreeTexture, screen2degreeVAO);
+            drawRectangles(rectShader, celsiusTexture, screen2celsiusVAO);
+			drawRectangles(rectShader, symbolTextures[currentSymbol], screen3VAO);
+        }
+        else {
+            drawRectangles(rectShader, darkGreyTexture, screen1VAO);
+            drawRectangles(rectShader, darkGreyTexture, screen2VAO);
+		    drawRectangles(rectShader, darkGreyTexture, screen3VAO);
+        }
+        
 		useFill = true;
         drawRectangles(rectShader, flapTexture, flapVAO);
 		drawCircle(circleShader, lampVAO);
@@ -149,6 +294,17 @@ int main()
 	deleteRectangles(bgVAO, bgVBO, bgTexture);
     deleteRectangles(acVAO, acVBO, acTexture);
     deleteRectangles(flapVAO, flapVBO, flapTexture);
+	deleteRectangles(screen1VAO, screen1VBO, screenTexture);
+	deleteRectangles(screen1num1VAO, screen1num1VBO, numberTextures[currentDigit1Screen1]);
+	deleteRectangles(screen1num2VAO, screen1num2VBO, numberTextures[currentDigit2Screen1]);
+	deleteRectangles(screen1degreeVAO, screen1degreeVBO, degreeTexture);
+	deleteRectangles(screen1celsiusVAO, screen1celsiusVBO, celsiusTexture);
+	deleteRectangles(screen2VAO, screen2VBO, screenTexture);
+	deleteRectangles(screen2num1VAO, screen2num1VBO, numberTextures[currentDigit1Screen2]);
+	deleteRectangles(screen2num2VAO, screen2num2VBO, numberTextures[currentDigit2Screen2]);
+	deleteRectangles(screen2degreeVAO, screen2degreeVBO, degreeTexture);
+	deleteRectangles(screen2celsiusVAO, screen2celsiusVBO, celsiusTexture);
+	deleteRectangles(screen3VAO, screen3VBO, symbolTextures[currentSymbol]);
 	deleteCircle(lampVAO, lampVBO);
 	glDeleteProgram(rectShader);
     glfwDestroyWindow(window);
